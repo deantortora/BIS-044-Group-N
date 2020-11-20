5339a58f309f854e5c02bee1dba77c9353fba43d
Yinglu-Wang <70137469+Yinglu-Wang@users.noreply.github.com>
2020-11-20 07:28:04 -0500

    Update Group project script.R

--- Group project script.R
+++ Group project script.R
@@ -1,3 +1,4 @@
+#We believe there will be an increase in Covid-19 cases in the United States by the end of the year (a second wave)
 #read the csv file and make a data frame
 read.csv("/Users/~/covid-19-data/us.csv",header = TRUE)
 DF<-read.csv("/Users/~/covid-19-data/us.csv")
@@ -15,4 +16,5 @@ plot(x=DF$date,
       y=DF$cases)
 
 
+#After looking at the visualization shown in the graph from March to November, the number of cases clearly does not decrease. This exponential line indicates that there was never a drop of cases, indicating there can be no second wave. 
 
